09e6875a7338225ab55ac9e9c6650423a9eb9c53
sabrinaduque <sabrinanduque@hotmail.com>
2023-08-17 17:27:04 -0300

    add app error

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,15 @@
-class UserController {
-    create(request, response) {
-        const { name, email, password } = request.body
+const AppError = require("../utils/AppError");
 
-        response.json({ name, email, password })
-    }
+class UserController {
+	create(request, response) {
+		const { name, email, password } = request.body
+
+		if (!name) {
+			throw new AppError("Nome é obrigatório!")
+		}
+		
+		response.status(201).json({ name, email, password })
+	}
 }
 
 module.exports = UserController
