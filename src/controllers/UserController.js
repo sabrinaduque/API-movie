1291115e0d4900c73d4a4d6d1ab916c07bd0ae62
sabrinaduque <sabrinanduque@hotmail.com>
2023-08-17 18:32:29 -0300

    add migration and create user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,15 +1,27 @@
+const knex = require("../database/knex")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError");
 
 class UserController {
-	create(request, response) {
-		const { name, email, password } = request.body
+  async create(request, response) {
+    const { name, email, password } = request.body
 
-		if (!name) {
-			throw new AppError("Nome é obrigatório!")
-		}
-		
-		response.status(201).json({ name, email, password })
-	}
+    const checkUserExist = await knex("users").where({ email:email })
+    
+    if (checkUserExist.length) {
+      throw new AppError("Este e-mail já está em uso.")
+    }
+
+    const hashedPassword = await hash(password, 8)
+  
+    const [id] = await knex("users").insert({
+      name,
+      email,
+      password:hashedPassword
+    })
+
+    response.status(201).json()
+  }
 }
 
 module.exports = UserController
